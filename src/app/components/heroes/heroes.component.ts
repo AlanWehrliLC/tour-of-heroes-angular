c09f5e86c3bef7f4a1ccb6d237b6d0a5de22654d
AlanWehrliLC <wehrlialan1@gmail.com>
2022-12-11 03:08:08 -0300

    Add navigation with routing

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../hero'
 import { HeroService } from '../../services/heroService/hero.service';
-import { MessageService } from '../../services/messageService/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,20 +11,12 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = []
 
-  selectedHero?: Hero
-
   constructor(
-    private heroService : HeroService,
-    private messageService : MessageService
+    private heroService : HeroService
   ){}
 
   ngOnInit(): void {
     this.getHeroes()
-  }
-
-  onSelect(hero: Hero) : void{
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
