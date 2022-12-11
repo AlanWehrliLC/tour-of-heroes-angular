a6180638e44e625567c91512ee8c48e11330c562
AlanWehrliLC <wehrlialan1@gmail.com>
2022-12-10 23:19:06 -0300

    Add services

---
+++ src/app/services/heroService/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from '../messageService/message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES)
+    this.messageService.add('HeroService: fetched heroes')
+    return heroes
+  }
+}
